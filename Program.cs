48fa303252ed6e64f903f0b8115212cf96e6838a
FidelskiyAndrey <fidelskiy@gmail.com>
2021-10-29 18:09:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11T1
+{
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Linear linear = new Linear(32, 52);
+                linear.Root();
+                Console.ReadKey();
+
+            }
+        }
+        struct Linear
+        {
+
+            double k;//Коэффициент 1
+            double b;//Коэффициент 2
+
+            public Linear(double k, double b)
+            {
+                this.k = k;
+                this.b = b;
+            }
+
+            public void Root()
+            {
+
+                double x = -b / k;
+                Console.WriteLine("Переменная x ={0}", x);
+            }
+
+        }
+    
+}
